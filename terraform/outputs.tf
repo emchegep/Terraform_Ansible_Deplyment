b02f0efa7928d4678f2985b2039313a153d2d088
Chege <app@apps-MacBook-Air.local>
2025-11-25 16:12:50 +0300

    added pipeline job for ansible

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
-output "instance_public_ip" {
+output "ec2_public_ip" {
   value       = aws_instance.webserver.public_ip
   description = "The public IP address of the EC2 instance"
 }
 
-output "instance_public_dns" {
+output "ec2_public_dns" {
   value       = aws_instance.webserver.public_dns
   description = "The public DNS name of the EC2 instance"
 }
